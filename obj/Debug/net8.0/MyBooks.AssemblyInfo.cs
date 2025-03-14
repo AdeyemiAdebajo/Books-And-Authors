17a4f6457e947ac1b5457e086990531e576b4356
Adeyemi <adeyemi.adebajo@canadorestudents.ca>
2025-03-14 13:35:21 -0400

    project ready for upload

--- obj/Debug/net8.0/MyBooks.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBooks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a0fac02d77b4d95b45c113399def3fc9f967d8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c818130f4dc1659eb1e5bb8305faa469165773c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
