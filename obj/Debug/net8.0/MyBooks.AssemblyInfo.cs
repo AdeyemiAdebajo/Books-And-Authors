9e811a652fc69edb58008c6ac540744f077df121
Adeyemi Adebajo <127756450+AdeyemiAdebajo@users.noreply.github.com>
2025-07-19 10:11:50 -0400

    fixe all buttons

--- obj/Debug/net8.0/MyBooks.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyBooks.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyBooks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df197c2e91a6247378ff2e965493f1fcec4d2ba2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e02a173843228feef3ce8333bd64a30771f4740")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyBooks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyBooks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
